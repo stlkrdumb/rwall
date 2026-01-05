fd9c8eccae20a59db634c6e6b943c0150638ae0e
stlkrdumb <nurairstalk@proton.me>
2026-01-05 19:53:03 +0700

    updating minipic options

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,42 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 import minipic from 'vite-plugin-minipic'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    minipic({
-      sharpOptions: {
-        png: { quality: 100 },
-        webp: { quality: 80 }
-      },
-      convert: [
-        {from: 'png', to: 'webp'}
-      ],
-      cache: true,
-    })
-  ],
+export default defineConfig(({ mode }) => {
+  console.log(mode)
+  return {
+    plugins: [
+      react(),
+      tailwindcss(),
+      minipic({
+        sharpOptions: {
+          png: { quality: 100 },
+          webp: { quality: 80 }
+        },
+        convert: [
+          {from: 'png', to: 'webp'}
+        ],
+        cache: mode === 'production' ? true : false,
+      })
+    ],
+  }
 })
+
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     tailwindcss(),
+//     minipic({
+//       sharpOptions: {
+//         png: { quality: 100 },
+//         webp: { quality: 80 }
+//       },
+//       convert: [
+//         {from: 'png', to: 'webp'}
+//       ],
+//       cache: true,
+//     })
+//   ],
+// })
