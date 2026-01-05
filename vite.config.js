9ffca1d8f58e59504e10833b1a3f328e18dc86a9
stlkrdumb <nurairstalk@proton.me>
2026-01-05 19:47:14 +0700

    adding new wallpaper

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 import minipic from 'vite-plugin-minipic'
@@ -16,7 +16,7 @@ export default defineConfig({
       convert: [
         {from: 'png', to: 'webp'}
       ],
-      cache: false
+      cache: true,
     })
   ],
 })
